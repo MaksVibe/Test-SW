3bc651cdea0003883b50bff6020156623a06738f
MaksVibe <parunovmaksym@gmail.com>
2021-12-10 16:46:19 +0200

    few features

--- src/scripts/addCard.js
+++ src/scripts/addCard.js
@@ -1,2 +1,24 @@
 import refs from "./refs";
+import { LOCAL_PRODUCTS } from "./localStorage";
+import card from "./assets/prod.json";
 
+const createMarkup = ({ sku, name, price, attribute }) => {
+  return `
+   <li class="products__card" id=${sku}>
+        <input
+          type="checkbox"
+          class="delete-checkbox"
+          name="checkbox"
+          id="check"
+        />
+        <ul>
+          <li class="products__sku">${sku}</li>
+          <li class="products__name">${name}</li>
+          <li class="products__price">${price}</li>
+          <li class="products__spec">${attribute}</li>
+        </ul>
+      </li>`
+};
+
+
+console.log(createMarkup(card));
